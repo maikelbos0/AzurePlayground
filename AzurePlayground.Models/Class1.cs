37e4c8bd65cf0f42fb74e5bd2675651a04034384
Maikel Bos <maikel.bos0@gmail.com>
2019-08-26 19:46:40 +0200

    Set up new projects for db test

---
+++ AzurePlayground.Models/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace AzurePlayground.Models {
+    public class User {
+        public int Id { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ AzurePlayground/Global.asax.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Web;
+
+namespace AzurePlayground {
+    public class Global : HttpApplication {
+        protected void Application_Start(object sender, EventArgs e) {
+
+        }
+    }
+}
