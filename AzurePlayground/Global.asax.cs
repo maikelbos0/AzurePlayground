d6521396dda606084e0a5d06779cc0d6bc2165cb
Maikel Bos <maikel.bos0@gmail.com>
2019-09-01 11:29:25 +0200

    Attempt at deploying Application Insights - Key in app service instead of source

---
+++ AzurePlayground/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.ApplicationInsights.Extensibility;
+using System;
+using System.Configuration;
+
+namespace AzurePlayground {
+    public class Global : System.Web.HttpApplication {
+        protected void Application_Start(object sender, EventArgs e) {
+            var instrumentationKey = ConfigurationManager.AppSettings["ApplicationInsights.InstrumentationKey"];
+
+            if (string.IsNullOrEmpty(instrumentationKey)) {
+                TelemetryConfiguration.Active.DisableTelemetry = true;
+            }
+            else {
+                TelemetryConfiguration.Active.InstrumentationKey = instrumentationKey;
+            }
+        }
+    }
+}
