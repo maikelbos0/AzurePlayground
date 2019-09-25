3db02c8fb74fed5f84a471aee98fd081ce363b9a
Maikel Bos <maikel.bos0@gmail.com>
2019-09-25 20:37:42 +0200

    Integration test: log in wrong and right

--- AzurePlayground.Tests/Integration/HomeControllerTests.cs
+++ AzurePlayground.Tests/Integration/HomeControllerTests.cs
@@ -8,7 +8,6 @@ using AzurePlayground.Utilities.Configuration;
 using AzurePlayground.Utilities.Mail;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -35,12 +34,14 @@ namespace AzurePlayground.Tests.Integration {
             UnityConfig.Container.RegisterInstance<IAppSettings>(_appSettings);
         }
 
+        private HomeController GetController() {
+            return UnityConfig.Container.Resolve<HomeController>();
+        }
+
         [TestMethod]
         public void HomeController_Registration_Activation_To_LogIn_Succeeds() {
-            var controller = UnityConfig.Container.Resolve<HomeController>();
-
             // Registration
-            var registrationResult = (ViewResult)controller.Register(new UserRegistration() {
+            var registrationResult = (ViewResult)GetController().Register(new UserRegistration() {
                 Email = "test@test.com",
                 Password = "test",
                 ConfirmPassword = "test"
@@ -51,12 +52,12 @@ namespace AzurePlayground.Tests.Integration {
 
             // Activation
             var activationCode = Regex.Match(_mailClient.SentMessages.Last().Body, "\\d+").Value;
-            var activationResult = (ViewResult)controller.Activate(activationCode, "test@test.com");
+            var activationResult = (ViewResult)GetController().Activate(activationCode, "test@test.com");
 
             activationResult.ViewName.Should().Be("Activated");
 
             // Log in
-            var logInResult = controller.LogIn(new UserLogIn() {
+            var logInResult = GetController().LogIn(new UserLogIn() {
                 Email = "test@test.com",
                 Password = "test"
             });
@@ -67,8 +68,6 @@ namespace AzurePlayground.Tests.Integration {
 
         [TestMethod]
         public void HomeController_RequestPasswordReset_ResetPassword_To_LogIn_Succeeds() {
-            var controller = UnityConfig.Container.Resolve<HomeController>();
-
             // Set up
             _playgroundContextFactory.Context.Users.Add(new User() {
                 Email = "test@test.com",
@@ -76,7 +75,7 @@ namespace AzurePlayground.Tests.Integration {
             });
 
             // Request password reset
-            var forgotPasswordResult = (ViewResult)controller.ForgotPassword(new UserPasswordResetRequest() {
+            var forgotPasswordResult = (ViewResult)GetController().ForgotPassword(new UserPasswordResetRequest() {
                 Email = "test@test.com"
             });
 
@@ -85,7 +84,7 @@ namespace AzurePlayground.Tests.Integration {
 
             // Reset password
             var token = Regex.Match(_mailClient.SentMessages.Last().Body, "(?<=\\=)\\w+(?=\")").Value;
-            var resetResult = (ViewResult)controller.ResetPassword(new UserPasswordReset() {
+            var resetResult = (ViewResult)GetController().ResetPassword(new UserPasswordReset() {
                 Email = "test@test.com",
                 PasswordResetToken = token,
                 NewPassword = "test",
@@ -95,7 +94,7 @@ namespace AzurePlayground.Tests.Integration {
             resetResult.ViewName.Should().Be("PasswordReset");
 
             // Log in
-            var logInResult = controller.LogIn(new UserLogIn() {
+            var logInResult = GetController().LogIn(new UserLogIn() {
                 Email = "test@test.com",
                 Password = "test"
             });
@@ -106,8 +105,6 @@ namespace AzurePlayground.Tests.Integration {
 
         [TestMethod]
         public void HomeController_ChangePassword_LogOut_To_LogIn_Succeeds() {
-            var controller = UnityConfig.Container.Resolve<HomeController>();
-
             // Set up
             _playgroundContextFactory.Context.Users.Add(new User() {
                 Email = "test@test.com",
@@ -119,7 +116,7 @@ namespace AzurePlayground.Tests.Integration {
             _authenticationProvider.Identity = "test@test.com";
 
             // Change password
-            var changePasswordResult = (ViewResult)controller.ChangePassword(new UserChangePassword() {
+            var changePasswordResult = (ViewResult)GetController().ChangePassword(new UserChangePassword() {
                 CurrentPassword = "test",
                 NewPassword = "hunter2",
                 ConfirmNewPassword = "hunter2"
@@ -127,15 +124,45 @@ namespace AzurePlayground.Tests.Integration {
             changePasswordResult.ViewName.Should().Be("PasswordChanged");
 
             // Log out
-            var logOutResult = controller.LogOut();
+            var logOutResult = GetController().LogOut();
 
             logOutResult.Should().BeOfType<RedirectToRouteResult>();
             _authenticationProvider.Identity.Should().BeNull();
 
             // Log in
-            var logInResult = controller.LogIn(new UserLogIn() {
+            var logInResult = GetController().LogIn(new UserLogIn() {
                 Email = "test@test.com",
                 Password = "hunter2"
+            });
+
+            logInResult.Should().BeOfType<RedirectToRouteResult>();
+            _authenticationProvider.Identity.Should().Be("test@test.com");
+        }
+
+        [TestMethod]
+        public void HomeController_FailedLogIn_To_LogIn_Succeeds() {
+            // Set up
+            _playgroundContextFactory.Context.Users.Add(new User() {
+                Email = "test@test.com",
+                PasswordHash = new byte[] { 248, 212, 57, 28, 32, 158, 38, 248, 82, 175, 53, 217, 161, 238, 108, 226, 48, 123, 118, 173 },
+                PasswordHashIterations = 1000,
+                PasswordSalt = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                IsActive = true
+            });
+
+            // Failed log in
+            var failedLogInResult = GetController().LogIn(new UserLogIn() {
+                Email = "test@test.com",
+                Password = "hunter2"
+            });
+
+            failedLogInResult.Should().BeOfType<ViewResult>();
+            _authenticationProvider.Identity.Should().BeNull();
+
+            // Log in
+            var logInResult = GetController().LogIn(new UserLogIn() {
+                Email = "test@test.com",
+                Password = "test"
             });
 
             logInResult.Should().BeOfType<RedirectToRouteResult>();
