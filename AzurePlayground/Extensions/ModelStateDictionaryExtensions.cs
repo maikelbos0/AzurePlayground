daebc9a23206ef2b6f75b2923d838bfe49602159
Maikel Bos <maikel.bos0@gmail.com>
2019-10-13 19:28:38 +0200

    Probable new merge function for model and command

--- AzurePlayground/Extensions/ModelStateDictionaryExtensions.cs
+++ AzurePlayground/Extensions/ModelStateDictionaryExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using AzurePlayground.Commands;
+using System;
 using System.Web.Mvc;
 
 namespace AzurePlayground.Extensions {
@@ -6,6 +7,19 @@ namespace AzurePlayground.Extensions {
         public static void Merge<TCommandParameter>(this ModelStateDictionary modelState, CommandResult<TCommandParameter> commandResult) {
             foreach (var commandError in commandResult.Errors) {
                 var propertyName = ExpressionHelper.GetExpressionText(commandError.Expression);
+                modelState.AddModelError(propertyName, commandError.Message);
+            }
+        }
+
+        [Obsolete("Implementation to follow", true)]
+        public static void Merge<TModel, TCommandParameter>(this ModelStateDictionary modelState, CommandResult<TCommandParameter> commandResult) {
+            foreach (var commandError in commandResult.Errors) {
+                var propertyName = ExpressionHelper.GetExpressionText(commandError.Expression);
+
+                if (typeof(TModel).GetProperty(propertyName) == null) {
+                    throw new InvalidOperationException($"Model type '{typeof(TModel)}' does not contain property '{propertyName}'");
+                }
+
                 modelState.AddModelError(propertyName, commandError.Message);
             }
         }
