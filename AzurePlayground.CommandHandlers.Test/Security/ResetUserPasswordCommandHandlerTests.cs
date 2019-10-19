18e312afc89a589356ba34c524b86393d1c90cb5
Maikel Bos <maikel.bos0@gmail.com>
2019-10-19 14:18:55 +0200

    Refactor reset password command tests

--- AzurePlayground.CommandHandlers.Test/Security/ResetUserPasswordCommandHandlerTests.cs
+++ AzurePlayground.CommandHandlers.Test/Security/ResetUserPasswordCommandHandlerTests.cs
@@ -1,157 +1,155 @@
-﻿//using AzurePlayground.CommandHandlers.Security;
-//using AzurePlayground.Commands.Security;
-//using AzurePlayground.Domain.Security;
-//using AzurePlayground.Test.Utilities;
-//using FluentAssertions;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using System;
-//using System.Linq;
+﻿using AzurePlayground.CommandHandlers.Security;
+using AzurePlayground.Commands.Security;
+using AzurePlayground.Domain.Security;
+using AzurePlayground.Test.Utilities;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
 
-//namespace AzurePlayground.CommandHandlers.Test.Security {
-//    [TestClass]
-//    public class ResetUserPasswordCommandHandlerTests {
-//        private readonly FakePlaygroundContextFactory _playgroundContextFactory = new FakePlaygroundContextFactory();
+namespace AzurePlayground.CommandHandlers.Test.Security {
+    [TestClass]
+    public class ResetUserPasswordCommandHandlerTests {
+        private readonly FakePlaygroundContextFactory _playgroundContextFactory = new FakePlaygroundContextFactory();
 
-//        [TestMethod]
-//        public void ResetUserPasswordCommandHandler_Succeeds() {
-//            var handler = new ResetUserPasswordCommandHandler(_playgroundContextFactory);
-//            var user = new User() {
-//                Email = "test@test.com",
-//                Password = new Password("test"),
-//                Status = UserStatus.Active,
-//                PasswordResetToken = new TemporaryPassword("test")
-//            };
-//            var command = new ResetUserPasswordCommand("test@test.com", "test", "test2");
+        [TestMethod]
+        public void ResetUserPasswordCommandHandler_Succeeds() {
+            var handler = new ResetUserPasswordCommandHandler(_playgroundContextFactory);
+            var command = new ResetUserPasswordCommand("test@test.com", "test", "test2");
+            var user = Substitute.For<User>();
+            user.Email.Returns("test@test.com");
+            user.Status.Returns(UserStatus.Active);
+            user.Password.Returns(new Password("test"));
+            user.PasswordResetToken.Returns(new TemporaryPassword("test"));
 
-//            _playgroundContextFactory.Context.Users.Add(user);
+            _playgroundContextFactory.Context.Users.Add(user);
 
-//            var result = handler.Execute(command);
+            var result = handler.Execute(command);
 
-//            result.Success.Should().BeTrue();
-//            user.UserEvents.Should().HaveCount(1);
-//            user.UserEvents.Single().Type.Should().Be(UserEventType.PasswordReset);
-//            user.Password.Verify("test2").Should().BeTrue();
-//        }
+            result.Success.Should().BeTrue();
+            user.Received().ResetPassword("test2");
+            user.DidNotReceive().ResetPasswordFailed();
+        }
 
-//        [TestMethod]
-//        public void ResetUserPasswordCommandHandler_Fails_For_Expired_Token() {
-//            var handler = new ResetUserPasswordCommandHandler(_playgroundContextFactory);
-//            var user = new User() {
-//                Email = "test@test.com",
-//                Password = new Password("test"),
-//                Status = UserStatus.Active,
-//                PasswordResetToken = new TemporaryPassword("test")
-//            };
-//            typeof(TemporaryPassword).GetProperty(nameof(TemporaryPassword.ExpiryDate)).SetValue(user.PasswordResetToken, DateTime.UtcNow.AddSeconds(-60));
-//            var command = new ResetUserPasswordCommand("test@test.com", "test", "test2");
+        [TestMethod]
+        public void ResetUserPasswordCommandHandler_Fails_For_Expired_Token() {
+            var handler = new ResetUserPasswordCommandHandler(_playgroundContextFactory);
+            var command = new ResetUserPasswordCommand("test@test.com", "test", "test2");
+            var user = Substitute.For<User>();
+            user.Email.Returns("test@test.com");
+            user.Status.Returns(UserStatus.Active);
+            user.Password.Returns(new Password("test"));
+            var token = new TemporaryPassword("test");
+            typeof(TemporaryPassword).GetProperty(nameof(TemporaryPassword.ExpiryDate)).SetValue(token, DateTime.UtcNow.AddSeconds(-60));
+            user.PasswordResetToken.Returns(token);
 
-//            _playgroundContextFactory.Context.Users.Add(user);
+            _playgroundContextFactory.Context.Users.Add(user);
 
-//            var result = handler.Execute(command);
+            var result = handler.Execute(command);
 
-//            result.Errors.Should().HaveCount(1);
-//            result.Errors[0].Expression.Should().BeNull();
-//            result.Errors[0].Message.Should().Be("The password reset link has expired; please request a new one");
-//            user.Password.Verify("test").Should().BeTrue();
-//            user.UserEvents.Should().HaveCount(1);
-//            user.UserEvents.Single().Type.Should().Be(UserEventType.FailedPasswordReset);
-//        }
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.Should().BeNull();
+            result.Errors[0].Message.Should().Be("The password reset link has expired; please request a new one");
+            user.DidNotReceive().ResetPassword(Arg.Any<string>());
+            user.Received().ResetPasswordFailed();
+        }
 
-//        [TestMethod]
-//        public void ResetUserPasswordCommandHandler_Fails_When_Already_Reset() {
-//            var handler = new ResetUserPasswordCommandHandler(_playgroundContextFactory);
-//            var user = new User() {
-//                Email = "test@test.com",
-//                Password = new Password("test"),
-//                Status = UserStatus.Active
-//            };
-//            var command = new ResetUserPasswordCommand("test@test.com", "test", "test2");
+        [TestMethod]
+        public void ResetUserPasswordCommandHandler_Fails_When_Already_Reset() {
+            var handler = new ResetUserPasswordCommandHandler(_playgroundContextFactory);
+            var command = new ResetUserPasswordCommand("test@test.com", "test", "test2");
+            var user = Substitute.For<User>();
+            user.Email.Returns("test@test.com");
+            user.Status.Returns(UserStatus.Active);
+            user.Password.Returns(new Password("test"));
+            user.PasswordResetToken.Returns(TemporaryPassword.None);
 
-//            _playgroundContextFactory.Context.Users.Add(user);
+            _playgroundContextFactory.Context.Users.Add(user);
 
-//            var result = handler.Execute(command);
+            var result = handler.Execute(command);
 
-//            result.Errors.Should().HaveCount(1);
-//            result.Errors[0].Expression.Should().BeNull();
-//            result.Errors[0].Message.Should().Be("The password reset link has expired; please request a new one");
-//            user.Password.Verify("test").Should().BeTrue();
-//            user.UserEvents.Should().HaveCount(1);
-//            user.UserEvents.Single().Type.Should().Be(UserEventType.FailedPasswordReset);
-//        }
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.Should().BeNull();
+            result.Errors[0].Message.Should().Be("The password reset link has expired; please request a new one");
+            user.DidNotReceive().ResetPassword(Arg.Any<string>());
+            user.Received().ResetPasswordFailed();
+        }
 
-//        [TestMethod]
-//        public void ResetUserPasswordCommandHandler_Fails_For_Incorrect_Token() {
-//            var handler = new ResetUserPasswordCommandHandler(_playgroundContextFactory);
-//            var user = new User() {
-//                Email = "test@test.com",
-//                Password = new Password("test"),
-//                Status = UserStatus.Active,
-//                PasswordResetToken = new TemporaryPassword("test")
-//            };
-//            var command = new ResetUserPasswordCommand("test@test.com", "wrong", "test2");
+        [TestMethod]
+        public void ResetUserPasswordCommandHandler_Fails_For_Incorrect_Token() {
+            var handler = new ResetUserPasswordCommandHandler(_playgroundContextFactory);
+            var command = new ResetUserPasswordCommand("test@test.com", "wrong", "test2");
+            var user = Substitute.For<User>();
+            user.Email.Returns("test@test.com");
+            user.Status.Returns(UserStatus.Active);
+            user.Password.Returns(new Password("test"));
+            user.PasswordResetToken.Returns(new TemporaryPassword("test"));
 
-//            _playgroundContextFactory.Context.Users.Add(user);
+            _playgroundContextFactory.Context.Users.Add(user);
 
-//            var result = handler.Execute(command);
+            var result = handler.Execute(command);
 
-//            result.Errors.Should().HaveCount(1);
-//            result.Errors[0].Expression.Should().BeNull();
-//            result.Errors[0].Message.Should().Be("The password reset link has expired; please request a new one");
-//            user.Password.Verify("test").Should().BeTrue();
-//            user.UserEvents.Should().HaveCount(1);
-//            user.UserEvents.Single().Type.Should().Be(UserEventType.FailedPasswordReset);
-//        }
+            result.Errors.Should().HaveCount(1);
+            result.Errors[0].Expression.Should().BeNull();
+            result.Errors[0].Message.Should().Be("The password reset link has expired; please request a new one");
+            user.DidNotReceive().ResetPassword(Arg.Any<string>());
+            user.Received().ResetPasswordFailed();
+        }
 
-//        [TestMethod]
-//        public void ResetUserPasswordCommandHandler_Throws_Exception_For_Inactive_User() {
-//            var handler = new ResetUserPasswordCommandHandler(_playgroundContextFactory);
-//            var user = new User() {
-//                Email = "test@test.com",
-//                Password = new Password("test"),
-//                Status = UserStatus.Inactive
-//            };
-//            var command = new ResetUserPasswordCommand("test@test.com", "test", "test2");
+        [TestMethod]
+        public void ResetUserPasswordCommandHandler_Throws_Exception_For_Inactive_User() {
+            var handler = new ResetUserPasswordCommandHandler(_playgroundContextFactory);
+            var command = new ResetUserPasswordCommand("test@test.com", "test", "test2");
+            var user = Substitute.For<User>();
+            user.Email.Returns("test@test.com");
+            user.Status.Returns(UserStatus.Inactive);
+            user.Password.Returns(new Password("test"));
+            user.PasswordResetToken.Returns(new TemporaryPassword("test"));
 
-//            _playgroundContextFactory.Context.Users.Add(user);
+            _playgroundContextFactory.Context.Users.Add(user);
 
-//            Action commandAction = () => {
-//                var result = handler.Execute(command);
-//            };
+            Action commandAction = () => {
+                var result = handler.Execute(command);
+            };
 
-//            commandAction.Should().Throw<InvalidOperationException>().WithMessage("Attempted to reset password for inactive user 'test@test.com'");
-//            user.Password.Verify("test").Should().BeTrue();
-//        }
+            commandAction.Should().Throw<InvalidOperationException>().WithMessage("Attempted to reset password for inactive user 'test@test.com'");
+            user.DidNotReceive().ResetPassword(Arg.Any<string>());
+            user.DidNotReceive().ResetPasswordFailed();
 
-//        [TestMethod]
-//        public void ResetUserPasswordCommandHandler_Throws_Exception_For_New_User() {
-//            var handler = new ResetUserPasswordCommandHandler(_playgroundContextFactory);
-//            var user = new User() {
-//                Email = "test@test.com",
-//                Password = new Password("test"),
-//                Status = UserStatus.New
-//            };
-//            var command = new ResetUserPasswordCommand("test@test.com", "test", "test2");
+        }
 
-//            _playgroundContextFactory.Context.Users.Add(user);
+        [TestMethod]
+        public void ResetUserPasswordCommandHandler_Throws_Exception_For_New_User() {
+            var handler = new ResetUserPasswordCommandHandler(_playgroundContextFactory);
+            var command = new ResetUserPasswordCommand("test@test.com", "test", "test2");
+            var user = Substitute.For<User>();
+            user.Email.Returns("test@test.com");
+            user.Status.Returns(UserStatus.New);
+            user.Password.Returns(new Password("test"));
+            user.PasswordResetToken.Returns(new TemporaryPassword("test"));
 
-//            Action commandAction = () => {
-//                var result = handler.Execute(command);
-//            };
+            _playgroundContextFactory.Context.Users.Add(user);
 
-//            commandAction.Should().Throw<InvalidOperationException>().WithMessage("Attempted to reset password for inactive user 'test@test.com'");
-//            user.Password.Verify("test").Should().BeTrue();
-//        }
+            Action commandAction = () => {
+                var result = handler.Execute(command);
+            };
 
-//        [TestMethod]
-//        public void ResetUserPasswordCommandHandler_Throws_Exception_For_Nonexistent_User() {
-//            var handler = new ResetUserPasswordCommandHandler(_playgroundContextFactory);
-//            var command = new ResetUserPasswordCommand("test@test.com", "test", "test2"); 
+            commandAction.Should().Throw<InvalidOperationException>().WithMessage("Attempted to reset password for inactive user 'test@test.com'");
+            user.DidNotReceive().ResetPassword(Arg.Any<string>());
+            user.DidNotReceive().ResetPasswordFailed();
 
-//            Action commandAction = () => {
-//                var result = handler.Execute(command);
-//            };
+        }
 
-//            commandAction.Should().Throw<InvalidOperationException>().WithMessage("Attempted to reset password for non-existent user 'test@test.com'");
-//        }
-//    }
-//}
+        [TestMethod]
+        public void ResetUserPasswordCommandHandler_Throws_Exception_For_Nonexistent_User() {
+            var handler = new ResetUserPasswordCommandHandler(_playgroundContextFactory);
+            var command = new ResetUserPasswordCommand("test@test.com", "test", "test2");
+
+            Action commandAction = () => {
+                var result = handler.Execute(command);
+            };
+
+            commandAction.Should().Throw<InvalidOperationException>().WithMessage("Attempted to reset password for non-existent user 'test@test.com'");
+        }
+    }
+}
