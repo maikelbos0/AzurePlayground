4a00a94e778cf0a7c57d3d52b7991c213c235c5f
Maikel Bos <maikel.bos0@gmail.com>
2019-10-19 08:50:57 +0200

    Implementing domain tests part 1

--- AzurePlayground.Domain.Tests/Security/UserTests.cs
+++ AzurePlayground.Domain.Tests/Security/UserTests.cs
@@ -8,14 +8,119 @@ namespace AzurePlayground.Domain.Tests.Security {
     [TestClass]
     public class UserTests {
         [TestMethod]
-        public void User_AddEvent_Adds_Event_Correctly() {
-            var user = new User();
+        public void User_Register_Succeeds() {
+            var user = new User("test@test.com", "test");
 
-            user.AddEvent(UserEventType.FailedLogIn);
+            user.ActivationCode.Should().NotBeNull();
+            user.Status.Should().Be(UserStatus.New);
+            user.UserEvents.Last().Type.Should().Be(UserEventType.Registered);
+            user.UserEvents.Last().Date.Should().BeCloseTo(DateTime.UtcNow);
+        }
 
-            user.UserEvents.Should().HaveCount(1);
-            user.UserEvents.First().Type.Should().Be(UserEventType.FailedLogIn);
-            user.UserEvents.First().Date.Should().BeCloseTo(DateTime.UtcNow);
+        [TestMethod]
+        public void User_LogIn_Succeeds() {
+            var user = new User("test@test.com", "test");
+
+            user.Activate();
+            user.GeneratePasswordResetToken();
+            user.LogIn();
+
+            user.PasswordResetToken.Should().Be(TemporaryPassword.None);
+            user.UserEvents.Last().Type.Should().Be(UserEventType.LoggedIn);
+            user.UserEvents.Last().Date.Should().BeCloseTo(DateTime.UtcNow);
+        }
+
+        [TestMethod]
+        public void User_LogInFailed_Succeeds() {
+            var user = new User("test@test.com", "test");
+
+            user.Activate();
+            user.LogInFailed();
+
+            user.UserEvents.Last().Type.Should().Be(UserEventType.FailedLogIn);
+            user.UserEvents.Last().Date.Should().BeCloseTo(DateTime.UtcNow);
+        }
+
+        [TestMethod]
+        public void User_LogOut_Succeeds() {
+            var user = new User("test@test.com", "test");
+
+            user.Activate();
+            user.LogOut();
+
+            user.UserEvents.Last().Type.Should().Be(UserEventType.LoggedOut);
+            user.UserEvents.Last().Date.Should().BeCloseTo(DateTime.UtcNow);
+        }
+
+        [TestMethod]
+        public void User_Activate_Succeeds() {
+            var user = new User("test@test.com", "test");
+
+            user.Activate();
+
+            user.ActivationCode.Should().BeNull();
+            user.Status.Should().Be(UserStatus.Active);
+            user.UserEvents.Last().Type.Should().Be(UserEventType.Activated);
+            user.UserEvents.Last().Date.Should().BeCloseTo(DateTime.UtcNow);
+        }
+
+        [TestMethod]
+        public void User_ActivationFailed_Succeeds() {
+            var user = new User("test@test.com", "test");
+
+            user.ActivationFailed();
+
+            user.ActivationCode.Should().NotBeNull();
+            user.Status.Should().Be(UserStatus.New);
+            user.UserEvents.Last().Type.Should().Be(UserEventType.FailedActivation);
+            user.UserEvents.Last().Date.Should().BeCloseTo(DateTime.UtcNow);
+        }
+
+        [TestMethod]
+        public void User_ChangePassword_Succeeds() {
+            var user = new User("test@test.com", "test");
+
+            user.Activate();
+            user.ChangePassword("new");
+
+            user.Password.Verify("new").Should().BeTrue();
+            user.UserEvents.Last().Type.Should().Be(UserEventType.PasswordChanged);
+            user.UserEvents.Last().Date.Should().BeCloseTo(DateTime.UtcNow);
+        }
+
+        [TestMethod]
+        public void User_ChangePasswordFailed_Succeeds() {
+            var user = new User("test@test.com", "test");
+
+            user.Activate();
+            user.ChangePasswordFailed();
+
+            user.UserEvents.Last().Type.Should().Be(UserEventType.FailedPasswordChange);
+            user.UserEvents.Last().Date.Should().BeCloseTo(DateTime.UtcNow);
+        }
+
+        [TestMethod]
+        public void User_ResetPassword_Succeeds() {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void User_ResetPasswordFailed_Succeeds() {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void User_GenerateActivationCode_Succeeds() {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void User_Deactivate_Succeeds() {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod] public void User_DeactivationFailed_Succeeds() {
+            throw new NotImplementedException();
         }
     }
 }
