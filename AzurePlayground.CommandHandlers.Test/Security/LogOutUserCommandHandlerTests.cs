e0fbf102083031b98f0dbd5593ed6adf8b5e461e
Maikel Bos <maikel.bos0@gmail.com>
2019-10-19 13:01:15 +0200

    Refactor log out tests

--- AzurePlayground.CommandHandlers.Test/Security/LogOutUserCommandHandlerTests.cs
+++ AzurePlayground.CommandHandlers.Test/Security/LogOutUserCommandHandlerTests.cs
@@ -1,76 +1,79 @@
-﻿//using AzurePlayground.CommandHandlers.Security;
-//using AzurePlayground.Domain.Security;
-//using AzurePlayground.Commands.Security;
-//using AzurePlayground.Test.Utilities;
-//using FluentAssertions;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using System;
-//using System.Linq;
+﻿using AzurePlayground.CommandHandlers.Security;
+using AzurePlayground.Domain.Security;
+using AzurePlayground.Commands.Security;
+using AzurePlayground.Test.Utilities;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
 
-//namespace AzurePlayground.CommandHandlers.Test.Security {
-//    [TestClass]
-//    public class LogOutUserCommandHandlerTests {
-//        private readonly FakePlaygroundContextFactory _playgroundContextFactory = new FakePlaygroundContextFactory();
+namespace AzurePlayground.CommandHandlers.Test.Security {
+    [TestClass]
+    public class LogOutUserCommandHandlerTests {
+        private readonly FakePlaygroundContextFactory _playgroundContextFactory = new FakePlaygroundContextFactory();
 
-//        [TestMethod]
-//        public void LogOutUserCommandHandler_Succeeds() {
-//            var handler = new LogOutUserCommandHandler(_playgroundContextFactory);
-//            var user = new User() {
-//                Email = "test@test.com",
-//                Status = UserStatus.Active
-//            };
-//            var command = new LogOutUserCommand("test@test.com");
+        [TestMethod]
+        public void LogOutUserCommandHandler_Succeeds() {
+            var handler = new LogOutUserCommandHandler(_playgroundContextFactory);
+            var command = new LogOutUserCommand("test@test.com");
+            var user = Substitute.For<User>();
+            user.Email.Returns("test@test.com");
+            user.Status.Returns(UserStatus.Active);
 
-//            _playgroundContextFactory.Context.Users.Add(user);
+            _playgroundContextFactory.Context.Users.Add(user);
 
-//            var result = handler.Execute(command);
+            var result = handler.Execute(command);
 
-//            result.Success.Should().BeTrue();
-//            user.UserEvents.Should().HaveCount(1);
-//            user.UserEvents.Single().Type.Should().Be(UserEventType.LoggedOut);
-//        }
+            result.Success.Should().BeTrue();
+            user.Received().LogOut();
+        }
 
-//        [TestMethod]
-//        public void LogOutUserCommandHandler_Throws_Exception_For_New_User() {
-//            var handler = new LogOutUserCommandHandler(_playgroundContextFactory);
-//            var command = new LogOutUserCommand("test@test.com");
-//            Action commandAction = () => {
-//                var result = handler.Execute(command);
-//            };
+        [TestMethod]
+        public void LogOutUserCommandHandler_Throws_Exception_For_New_User() {
+            var handler = new LogOutUserCommandHandler(_playgroundContextFactory);
+            var command = new LogOutUserCommand("test@test.com");
+            var user = Substitute.For<User>();
+            user.Email.Returns("test@test.com");
+            user.Status.Returns(UserStatus.New);
 
-//            _playgroundContextFactory.Context.Users.Add(new User() {
-//                Email = "test@test.com",
-//                Status = UserStatus.New
-//            });
+            Action commandAction = () => {
+                var result = handler.Execute(command);
+            };
 
-//            commandAction.Should().Throw<InvalidOperationException>().WithMessage("Attempted to log out inactive user 'test@test.com'");
-//        }
+            _playgroundContextFactory.Context.Users.Add(user);
 
-//        [TestMethod]
-//        public void LogOutUserCommandHandler_Throws_Exception_For_Inactive_User() {
-//            var handler = new LogOutUserCommandHandler(_playgroundContextFactory);
-//            var command = new LogOutUserCommand("test@test.com");
-//            Action commandAction = () => {
-//                var result = handler.Execute(command);
-//            };
+            commandAction.Should().Throw<InvalidOperationException>().WithMessage("Attempted to log out inactive user 'test@test.com'");
+            user.DidNotReceive().LogOut();
+        }
 
-//            _playgroundContextFactory.Context.Users.Add(new User() {
-//                Email = "test@test.com",
-//                Status = UserStatus.Inactive
-//            });
+        [TestMethod]
+        public void LogOutUserCommandHandler_Throws_Exception_For_Inactive_User() {
+            var handler = new LogOutUserCommandHandler(_playgroundContextFactory);
+            var command = new LogOutUserCommand("test@test.com");
+            var user = Substitute.For<User>();
+            user.Email.Returns("test@test.com");
+            user.Status.Returns(UserStatus.Inactive);
 
-//            commandAction.Should().Throw<InvalidOperationException>().WithMessage("Attempted to log out inactive user 'test@test.com'");
-//        }
+            Action commandAction = () => {
+                var result = handler.Execute(command);
+            };
 
-//        [TestMethod]
-//        public void LogOutUserCommandHandler_Throws_Exception_For_Nonexistent_User() {
-//            var handler = new LogOutUserCommandHandler(_playgroundContextFactory);
-//            var command = new LogOutUserCommand("test@test.com");
-//            Action commandAction = () => {
-//                var result = handler.Execute(command);
-//            };
+            _playgroundContextFactory.Context.Users.Add(user);
 
-//            commandAction.Should().Throw<InvalidOperationException>().WithMessage("Attempted to log out non-existent user 'test@test.com'");
-//        }
-//    }
-//}
+            commandAction.Should().Throw<InvalidOperationException>().WithMessage("Attempted to log out inactive user 'test@test.com'");
+            user.DidNotReceive().LogOut();
+
+        }
+
+        [TestMethod]
+        public void LogOutUserCommandHandler_Throws_Exception_For_Nonexistent_User() {
+            var handler = new LogOutUserCommandHandler(_playgroundContextFactory);
+            var command = new LogOutUserCommand("test@test.com");
+            Action commandAction = () => {
+                var result = handler.Execute(command);
+            };
+
+            commandAction.Should().Throw<InvalidOperationException>().WithMessage("Attempted to log out non-existent user 'test@test.com'");
+        }
+    }
+}
