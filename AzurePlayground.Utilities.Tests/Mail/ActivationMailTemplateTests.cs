09bd43e8b6fcdf04ea37946bb6d83117d1e1d4a2
Maikel Bos <maikel.bos0@gmail.com>
2019-10-19 14:49:59 +0200

    Template tests

---
+++ AzurePlayground.Utilities.Tests/Mail/ActivationMailTemplateTests.cs
@@ -0,0 +1,40 @@
+﻿using AzurePlayground.Test.Utilities;
+using AzurePlayground.Utilities.Mail;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace AzurePlayground.Utilities.Tests.Mail {
+    [TestClass]
+    public class ActivationMailTemplateTests {
+        private readonly FakeAppSettings _appSettings = new FakeAppSettings() {
+            Settings = new Dictionary<string, string>() {
+                { "Application.BaseUrl", "http://localhost/" }
+            }
+        };
+
+        [TestMethod]
+        public void ActivationMailTemplate_Generates_Correct_Subject() {
+            var template = new ActivationMailTemplate(_appSettings);
+            var parameters = new ActivationMailTemplateParameters("test@test.com", 12345);
+
+            template.GetMessage(parameters, "test@test.com").Subject.Should().Be("Please activate your account");
+        }
+
+        [TestMethod]
+        public void ActivationMailTemplate_Generates_Correct_Body() {
+            var template = new ActivationMailTemplate(_appSettings);
+            var parameters = new ActivationMailTemplateParameters("test@test.com", 12345);
+
+            template.GetMessage(parameters, "test@test.com").Body.Should().Be("<p>Please <a href=\"http://localhost/Home/Activate/?activationCode=12345&email=test%40test.com\">click here to activate your account</a>.</p><p>Activation code: 12345</p>");
+        }
+
+        [TestMethod]
+        public void ActivationMailTemplate_Uses_Correct_Address() {
+            var template = new ActivationMailTemplate(_appSettings);
+            var parameters = new ActivationMailTemplateParameters("test@test.com", 12345);
+
+            template.GetMessage(parameters, "test@test.com").To.Should().Be("test@test.com");
+        }
+    }
+}
---
+++ AzurePlayground.Utilities.Tests/Mail/PasswordResetMailTemplateTests.cs
@@ -0,0 +1,40 @@
+﻿using AzurePlayground.Test.Utilities;
+using AzurePlayground.Utilities.Mail;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace AzurePlayground.Utilities.Tests.Mail {
+    [TestClass]
+    public class PasswordResetMailTemplateTests {
+        private readonly FakeAppSettings _appSettings = new FakeAppSettings() {
+            Settings = new Dictionary<string, string>() {
+                { "Application.BaseUrl", "http://localhost/" }
+            }
+        };
+
+        [TestMethod]
+        public void PasswordResetMailTemplate_Generates_Correct_Subject() {
+            var template = new PasswordResetMailTemplate(_appSettings);
+            var parameters = new PasswordResetMailTemplateParameters("test@test.com", "asdfasdf");
+
+            template.GetMessage(parameters, "test@test.com").Subject.Should().Be("Your password reset request");
+        }
+
+        [TestMethod]
+        public void PasswordResetMailTemplate_Generates_Correct_Body() {
+            var template = new PasswordResetMailTemplate(_appSettings);
+            var parameters = new PasswordResetMailTemplateParameters("test@test.com", "asdfasdf");
+
+            template.GetMessage(parameters, "test@test.com").Body.Should().Be("<p>Please <a href=\"http://localhost/Home/ResetPassword/?email=test%40test.com&token=asdfasdf\">click here to reset your password</a>.</p>");
+        }
+
+        [TestMethod]
+        public void PasswordResetMailTemplate_Uses_Correct_Address() {
+            var template = new PasswordResetMailTemplate(_appSettings);
+            var parameters = new PasswordResetMailTemplateParameters("test@test.com", "asdfasdf");
+
+            template.GetMessage(parameters, "test@test.com").To.Should().Be("test@test.com");
+        }
+    }
+}
