89349737e11a979869c73a5b859607c45f90614c
Maikel Bos <maikel.bos0@gmail.com>
2019-10-20 16:29:01 +0200

    Repository tests

---
+++ AzurePlayground.Repositories.Tests/Security/UserRepositoryTests.cs
@@ -0,0 +1,104 @@
+﻿using AzurePlayground.Domain.Security;
+using AzurePlayground.Repositories.Security;
+using AzurePlayground.Test.Utilities;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AzurePlayground.Repositories.Tests.Security {
+    [TestClass]
+    public class UserRepositoryTests {
+        private readonly FakePlaygroundContext _context = new FakePlaygroundContext();
+
+        [TestInitialize]
+        public void Initialize() {
+            _context.Users.Add(new User("first@test.com", "test"));
+            _context.Users.Add(new User("test@test.com", "test"));
+            _context.Users.Add(new User("ANOTHER@TEST.COM", "test"));
+            _context.Users.Add(new User("wrong@test.com", "test"));
+            _context.Users.Add(new User("accent@test.com", "test"));
+            _context.Users.Add(new User("áççënt@test.com", "test"));
+
+            var id = 1;
+
+            foreach (var user in _context.Users) {
+                typeof(User).GetProperty("Id").SetValue(user, id++);
+            }
+        }
+
+        [TestMethod]
+        public void UserRepository_TryGetByEmail_Succeeds() {
+            var repository = new UserRepository(_context);
+
+            var user = repository.TryGetByEmail("test@test.com");
+
+            user.Should().NotBeNull();
+            user.Email.Should().Be("test@test.com");
+        }
+
+        [TestMethod]
+        public void UserRepository_TryGetByEmail_Is_Case_Insensitive() {
+            var repository = new UserRepository(_context);
+
+            var user = repository.TryGetByEmail("another@test.com");
+
+            user.Should().NotBeNull();
+            user.Email.Should().Be("ANOTHER@TEST.COM");
+        }
+
+        [TestMethod]
+        public void UserRepository_TryGetByEmail_Is_Accent_Sensitive() {
+            var repository = new UserRepository(_context);
+
+            var user = repository.TryGetByEmail("áççënt@test.com");
+
+            user.Should().NotBeNull();
+            user.Email.Should().Be("áççënt@test.com");
+        }
+
+        [TestMethod]
+        public void UserRepository_TryGetByEmail_Returns_Null_For_Nonexistent_Email() {
+            var repository = new UserRepository(_context);
+
+            var user = repository.TryGetByEmail("nobody@test.com");
+
+            user.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void UserRepository_TryGetByEmail_Does_Not_Save() {
+            var repository = new UserRepository(_context);
+
+            repository.TryGetByEmail("test@test.com");
+
+            _context.CallsToSaveChanges.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void UserRepository_Add_Succeeds() {
+            var repository = new UserRepository(_context);
+            var user = new User("new@test.com", "test");
+
+            repository.Add(user);
+
+            _context.Users.Should().Contain(user);
+        }
+
+        [TestMethod]
+        public void UserRepository_Add_Saves() {
+            var repository = new UserRepository(_context);
+
+            repository.Add(new User("new@test.com", "test"));
+
+            _context.CallsToSaveChanges.Should().Be(1);
+        }
+
+        [TestMethod]
+        public void UserRepository_Update_Saves() {
+            var repository = new UserRepository(_context);
+
+            repository.Update();
+
+            _context.CallsToSaveChanges.Should().Be(1);
+        }
+    }
+}
--- AzurePlayground.Test.Utilities/FakePlaygroundContext.cs
+++ AzurePlayground.Test.Utilities/FakePlaygroundContext.cs
@@ -5,12 +5,15 @@ using Security = AzurePlayground.Domain.Security;
 namespace AzurePlayground.Test.Utilities {
     public class FakePlaygroundContext : IPlaygroundContext {
         public IDbSet<Security.User> Users { get; set; } = new FakeDbSet<Security.User>();
+        public int CallsToSaveChanges { get; private set; }
 
         public void Dispose() {
             SaveChanges();
         }
 
         public int SaveChanges() {
+            CallsToSaveChanges++;
+
             // For now, implementation is not needed
             return 0;
         }
