216801b82be96002c6cb62d666b7bae4825d2d6f
Maikel Bos <maikel.bos0@gmail.com>
2019-10-24 18:06:34 +0200

    Disable buttons on submit for html only forms

--- AzurePlayground/App_Start/BundleConfig.cs
+++ AzurePlayground/App_Start/BundleConfig.cs
@@ -15,6 +15,7 @@ namespace AzurePlayground.App_Start {
 
             bundles.Add(new ScriptBundle("~/Scripts/app").Include(
                 "~/Scripts/application/jquery-ajaxform.js",
+                "~/Scripts/application/jquery-htmlform.js",
                 "~/Scripts/application/toastr-defaults.js"));
 
             bundles.Add(new StyleBundle("~/Content/css-bootstrap").Include(
