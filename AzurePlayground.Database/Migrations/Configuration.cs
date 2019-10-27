c26f566627844c09c4f733ba502312b4f9513de2
Maikel Bos <maikel.bos0@gmail.com>
2019-10-27 07:32:11 +0100

    More users

--- AzurePlayground.Database/Migrations/Configuration.cs
+++ AzurePlayground.Database/Migrations/Configuration.cs
@@ -3,11 +3,17 @@ namespace AzurePlayground.Database.Migrations {
     using AzurePlayground.Domain.Security;
     using System;
     using System.Data.Entity.Migrations;
+    using System.Linq;
 
     public sealed class Configuration : DbMigrationsConfiguration<PlaygroundContext> {
         protected override void Seed(PlaygroundContext context) {
             SeedEntityType<UserEventType, UserEventTypeEntity>(context);
             SeedEntityType<UserStatus, UserStatusEntity>(context);
+
+            AddOrUpdateUser(context, "example@test.com", "I am example", null, true);
+            AddOrUpdateUser(context, "anon@test.com", null, null, false);
+            AddOrUpdateUser(context, "you@test.com", "You", "Who are you?", false);
+            AddOrUpdateUser(context, "another@test.com", "Another", "One bites the dust", true);
         }
 
         private void SeedEntityType<TEnum, TReferenceEntity>(PlaygroundContext context)
@@ -17,6 +23,22 @@ namespace AzurePlayground.Database.Migrations {
             foreach (var entity in ReferenceEntityExtensions.GetValues<TEnum, TReferenceEntity>()) {
                 context.Set<TReferenceEntity>().AddOrUpdate(entity);
             }
+
+            context.SaveChanges();
+        }
+
+        private void AddOrUpdateUser(PlaygroundContext context, string email, string displayName, string description, bool showEmail) {
+            var user = context.Users.SingleOrDefault(u => u.Email.Equals(email, StringComparison.InvariantCultureIgnoreCase));
+
+            if (user == null) {
+                user = new User(email, new Random().Next().ToString());
+                context.Users.Add(user);
+            }
+
+            user.Activate();
+            user.DisplayName = displayName;
+            user.Description = description;
+            user.ShowEmail = showEmail;
 
             context.SaveChanges();
         }
